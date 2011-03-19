c5f6e5f45aeeb68e9c7b8de8602017724a06fef0
Christophe Narbonne <christophe31@gmail.com>
2011-03-19 11:19:14 +0100

    Add Comments objects.

--- SupInBlog-ejb/src/java/com/supinblog/services/entities/Comment.java
+++ SupInBlog-ejb/src/java/com/supinblog/services/entities/Comment.java
@@ -7,12 +7,11 @@ package com.supinblog.services.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 
 /**
@@ -20,58 +19,26 @@ import javax.persistence.Temporal;
  * @author popi
  */
 @Entity
-public class UserAccount implements Serializable {
-    @OneToMany(mappedBy = "author")
-    private List<Comment> comments;
+public class Comment implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
-    private String userName;
-    private String password;
-    private String email;
-    
+    @ManyToOne
+    private UserAccount author;
+    private String content;
     @Temporal(javax.persistence.TemporalType.DATE)
-    private Date dateOfBirth;
+    private Date creationDate;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date modificationDate;
     
+            
     @Override
     public String toString() {
-        return "com.supinblog.services.entities.User[ id=" + id + " ]";
+        return "com.supinblog.services.entities.Comment[ id=" + id + " ]";
     }
     
 // <editor-fold defaultstate="collapsed" desc="Generated, getters, setters"> 
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 
     public Long getId() {
         return id;
@@ -91,14 +58,14 @@ public class UserAccount implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof UserAccount)) {
+        if (!(object instanceof Comment)) {
             return false;
         }
-        UserAccount other = (UserAccount) object;
+        Comment other = (Comment) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
     }
-// </editor-fold> 
+// </editor-fold>
 }
